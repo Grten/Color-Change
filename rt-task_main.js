2f639da19fb093280e6de4c09f89b0bebf8499fc
Grten <83755931+Grten@users.noreply.github.com>
2021-05-09 14:41:23 +1200

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -1,3 +1,8 @@
+/* Change 1: Adding the image hosting site */
+// define the site that hosts stimuli images
+// usually https://<your-github-username>.github.io/<your-experiment-name>/
+var repo_site = "https://kywch.github.io/Simple-RT-Task/"; 
+
 /* create timeline */
 var timeline = [];
 
@@ -9,9 +14,6 @@ var welcome_block = {
 timeline.push(welcome_block);
 
 /* define instructions trial */
-// define the site that hosts stimuli images
-// usually https://<your-github-username>.github.io/<your-experiment-name>/
-        var repo_site = "https://kywch.github.io/Color-Change/Colour";
 var instructions = {
     type: "html-keyboard-response",
     stimulus: "<p>In this experiment, a circle will appear in the center " +
@@ -19,12 +21,12 @@ var instructions = {
         "press the letter F on the keyboard as fast as you can.</p>" +
         "<p>If the circle is <strong>orange</strong>, press the letter J " +
         "as fast as you can.</p>" +
-        "<div style='width: 700px;'>"+
-        "<div style='float: left;'><img src='" + repo_site + "img/blue.png'></img>" +
+        "<div style='width: 700px;'>" +
+        "<div style='float: left;'><img src='" + repo_site + "img/blue.png'></img>" + // Change 2: Adding `repo_site` in `instructions`
         "<p class='small'><strong>Press the F key</strong></p></div>" +
-        "<div class='float: right;'><img src='" + repo_site + "img/orange.png'></img>" +
+        "<div class='float: right;'><img src='" + repo_site + "img/orange.png'></img>" + // Change 2: Adding `repo_site` in `instructions`
         "<p class='small'><strong>Press the J key</strong></p></div>" +
-        "</div>"+
+        "</div>" +
         "<p>Press any key to begin.</p>",
     post_trial_gap: 2000
 };
@@ -32,19 +34,32 @@ timeline.push(instructions);
 
 /* test trials */
 
-var test_stimuli = [
-    { stimulus: repo_site + "img/blue.png", data: { test_part: 'test', correct_response: 'f' } },
-    { stimulus: repo_site + "img/orange.png", data: { test_part: 'test', correct_response: 'j' } }
+var test_stimuli = [{
+        stimulus: repo_site + "img/blue.png", // Change 3: Adding `repo_site` in `test_stimuli`
+        data: {
+            test_part: 'test',
+            correct_response: 'f'
+        }
+    },
+    {
+        stimulus: repo_site + "img/orange.png", // Change 3: Adding `repo_site` in `test_stimuli`
+        data: {
+            test_part: 'test',
+            correct_response: 'j'
+        }
+    }
 ];
 
 var fixation = {
     type: 'html-keyboard-response',
     stimulus: '<div style="font-size:60px;">+</div>',
     choices: jsPsych.NO_KEYS,
-    trial_duration: function(){
-    return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
+    trial_duration: function () {
+        return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
     },
-    data: {test_part: 'fixation'}
+    data: {
+        test_part: 'fixation'
+    }
 }
 
 var test = {
@@ -52,8 +67,8 @@ var test = {
     stimulus: jsPsych.timelineVariable('stimulus'),
     choices: ['f', 'j'],
     data: jsPsych.timelineVariable('data'),
-    on_finish: function(data){
-    data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
+    on_finish: function (data) {
+        data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
     },
 }
 
@@ -69,16 +84,20 @@ timeline.push(test_procedure);
 
 var debrief_block = {
     type: "html-keyboard-response",
-    stimulus: function() {
+    stimulus: function () {
 
-    var trials = jsPsych.data.get().filter({test_part: 'test'});
-    var correct_trials = trials.filter({correct: true});
-    var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
-    var rt = Math.round(correct_trials.select('rt').mean());
+        var trials = jsPsych.data.get().filter({
+            test_part: 'test'
+        });
+        var correct_trials = trials.filter({
+            correct: true
+        });
+        var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
+        var rt = Math.round(correct_trials.select('rt').mean());
 
-    return "<p>You responded correctly on "+accuracy+"% of the trials.</p>"+
-    "<p>Your average response time was "+rt+"ms.</p>"+
-    "<p>Press any key to complete the experiment. Thank you!</p>";
+        return "<p>You responded correctly on " + accuracy + "% of the trials.</p>" +
+            "<p>Your average response time was " + rt + "ms.</p>" +
+            "<p>Press any key to complete the experiment. Thank you!</p>";
 
     }
 };
