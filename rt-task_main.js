9f2472a720e13d8a222c134d6252bbd68ca1aaa4
Grten <83755931+Grten@users.noreply.github.com>
2021-05-12 19:12:57 +1200

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -68,7 +68,7 @@ var test = {
 var test_procedure = {
     timeline: [fixation, test],
     timeline_variables: test_stimuli,
-    repetitions: 5,
+    repetitions: 100,
     randomize_order: true
 }
 timeline.push(test_procedure);
@@ -87,8 +87,8 @@ var debrief_block = {
         });
         var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
         var rt = Math.round(correct_trials.select('rt').mean());
-        if ('rt' > 0)
-            'rt' = 1000
+        if (rt > 0)
+            rt + 1000
         return "<p>You responded correctly on " + accuracy + "% of the trials.</p>" +
             "<p>Your average response time was " + rt + "ms.</p>" +
             "<p>Press any key to complete the experiment. Thank you!</p>";
